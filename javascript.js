7d2641435d2c93f17d3eb1324c2bfa352be68299
Beata Kwiecińska <beatainspiro@gmail.com>
2022-07-07 13:20:04 +0200

    js file

---
+++ javascript.js
@@ -0,0 +1,28 @@
+const age = 40;
+
+let number = 12;
+number = 14;
+// console.log(number);
+
+let name;
+name = "beata";
+// console.log(name);
+
+let test;
+test = true;
+console.log(test);
+
+let colours = [];
+colours = ['pink', 'white', 'black'];
+console.log(colours[1]);
+
+let book = {
+    pages: 12,
+    hardCover: true,
+    title: 'bible'
+};
+console.log(book);
+
+
+
+
