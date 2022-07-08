ae35cb34afbe5c861b8dac5feb291d27c4268bcf
Beata Kwiecińska <beatainspiro@gmail.com>
2022-07-08 12:31:04 +0200

    3day

---
+++ 3day.js
@@ -0,0 +1,26 @@
+// for (let i=0; i<40; i++) {
+//     console.log(i+1);
+// }
+
+
+// let diesel = false;
+// console.log(diesel);
+// if (diesel) {
+//     console.log('tank oil');
+// } else {
+//     console.log('tank gas');
+// }
+
+// let age = 4;
+// if ( age>=18 ) {
+//     console.log ('You can drink beer');
+// } else {
+//     console.log ('You can not drink beer');
+// }
+
+for (let i=1; i<=40; i++) {
+    if (i%2) {
+    } else {
+        console.log(i);
+    }
+}
