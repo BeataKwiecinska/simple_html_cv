3a961c9264da977bda630f6441bd829ef3334aa7
Beata Kwiecińska <beatainspiro@gmail.com>
2022-07-13 19:44:36 +0200

    wolfs update and negative numbers

--- codewars/8kyu/wolfs.js
+++ codewars/8kyu/wolfs.js
@@ -1,17 +1,18 @@
-let queue  = ['sheep', 'sheep', 'sheep', 'sheep', 'wolf', 'sheep', 'sheep', 'sheep'];
+let queue  = ['sheep', 'sheep', 'sheep', 'sheep', 'sheep', 'wolf', 'sheep', 'sheep'];
 
 function warnTheSheep(q) {
     if (q[q.length - 1] === 'wolf') {
         return 'Pls go away and stop eating my sheep';
     } else {
-        let j = 1;
-        for (let i=q.length - 1; i>0; i--) {
-           j += 1;
+        for (let i = 0; i <= q.length; i++) {
             if (q[i] === 'wolf') {
-                return `Oi! Sheep number ${j-2}! You are about to be eaten by a wolf!`
+                if (q[i+1] === 'sheep') {
+                    return `Oi! Sheep number ${q.length - (i+1)}! You are about to be eaten by a wolf!`;
+                }
             }
-        }
-        
+        }    
     }
+        
+
 }
 console.log(warnTheSheep(queue));
