cf481ccc57ad2e0ca64210de34132b5fba33df6d
Beata Kwiecińska <beatainspiro@gmail.com>
2022-07-19 20:29:06 +0200

    new tasks

---
+++ codewars/8kyu/multiplesOfNumber.js
@@ -0,0 +1,13 @@
+// function findMultiples(integer, limit) {
+//   let multiples = [];
+//   for (let i = 1; i <= limit; i++) {
+//     let current = integer*i;
+//     if (current <= limit) {
+//       multiples.push(current); // dodaje element i na koniec tabeli multiples
+//     }
+//   }
+//   return multiples;
+// }
+
+findMultiples2 = (integer, limit) => 
+console.log(findMultiples(2, 10))
